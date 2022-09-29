429ba023b75cef55b00ad544f6fe414b00843265
Sabarno Chatterjee <106575210+Sabarno-Chatterjee@users.noreply.github.com>
2022-09-29 21:37:25 +0530

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,1 +1,18 @@
-
+def calculations(player_total, dealer_total):
+    if player_total > 21 and dealer_total > 21:
+        return "That's a double bust, regame time!"
+    elif player_total == 21 and dealer_total < 21:
+        return "You win."
+    elif player_total < 21 and dealer_total == 21:
+        return "Computer wins!"
+    elif player_total == dealer_total:
+        return "It's a draw."
+    elif player_total > 21 and dealer_total <= 21:
+        return "That's a bust, computer wins!"
+    elif dealer_total > 21 and player_total <= 21:
+        return "The comp busted, you win!"
+    elif dealer_total < 21 and player_total < 21:
+        if dealer_total > player_total:
+            return "The comp wins."
+        else:
+            return "You win!"
